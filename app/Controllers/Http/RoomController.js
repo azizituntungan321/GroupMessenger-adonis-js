b984878e75e42f7371df2a73753994b098aa3570
azizi <azizimukmin16@gmail.com>
2019-07-13 21:19:21 +0700

    GroupMessenger

--- app/Controllers/Http/RoomController.js
+++ app/Controllers/Http/RoomController.js
@@ -5,17 +5,43 @@ const Database = use('Database')
 
 class RoomController {
 
+  // async show({ params, request, response, view }) {
+  //   const { id } = params
+  //   const member = await Database
+  //     .select('paticipants.room_id', 'paticipants.user_id', 'rooms.name')
+  //     .table('paticipants')
+  //     .innerJoin('rooms', 'rooms.id', 'paticipants.room_id')
+  //     .innerJoin('users', 'users.id', 'paticipants.user_id')
+  //     .where({
+  //       'users.id': id
+  //     })
+  //     .orderBy('paticipants.id','desc')
+
+  //   if (member.length > 0) {
+  //     response.send({
+  //       message: 'Data found!',
+  //       data: member
+  //     })
+  //   } else {
+  //     response.send({
+  //       message: 'Data not found!'
+  //     })
+  //   }
+  // }
+
   async show({ params, request, response, view }) {
     const { id } = params
     const member = await Database
-      .select('paticipants.room_id', 'paticipants.user_id', 'rooms.name')
+      .select('paticipants.room_id', 'paticipants.user_id', 'rooms.name','chats.chat','chats.created_at')
       .table('paticipants')
       .innerJoin('rooms', 'rooms.id', 'paticipants.room_id')
       .innerJoin('users', 'users.id', 'paticipants.user_id')
+      .innerJoin('chats','rooms.id','chats.room_id')
       .where({
         'users.id': id
       })
-      .orderBy('paticipants.id','desc')
+      .orderBy('chats.id','desc')
+      .groupBy('chats.room_id')
 
     if (member.length > 0) {
       response.send({
@@ -28,7 +54,7 @@ class RoomController {
       })
     }
   }
-
+  
   async showLastMessage({ params, request, response, view }) {
     const { id, room } = params
     const member = await Database
